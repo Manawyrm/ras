0ca9939960a13d0f80d481e2568b4b485fd2f2b8
Manawyrm <git@tbspace.de>
2023-10-22 22:26:59 +0000

    configure TCP_NODELAY for telnet sockets

--- src/telnet.c
+++ src/telnet.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <netinet/tcp.h>
 #include <osmocom/core/select.h>
 #include <osmocom/core/socket.h>
 
@@ -7,6 +11,7 @@ static struct osmo_fd telnet_ofd;
 
 int telnet_init(void *telnet_rx_cb, char* hostname, uint16_t port, struct osmo_fd **ofd)
 {
+    int ret, on;
     int rc = -1;
     telnet_ofd.cb = telnet_rx_cb;
     telnet_ofd.when = OSMO_FD_READ | OSMO_FD_EXCEPT;
@@ -14,6 +19,12 @@ int telnet_init(void *telnet_rx_cb, char* hostname, uint16_t port, struct osmo_f
                             SOCK_STREAM, IPPROTO_TCP,
                             hostname, port,
                             OSMO_SOCK_F_CONNECT);
+
+    ret = setsockopt(telnet_ofd.fd, IPPROTO_TCP, TCP_NODELAY, &on, sizeof(on));
+    if (ret != 0) {
+        fprintf(stderr, "Failed to set TCP_NODELAY: %s\n", strerror(errno));
+        return rc;
+    }
 
     *ofd = &telnet_ofd;
 
